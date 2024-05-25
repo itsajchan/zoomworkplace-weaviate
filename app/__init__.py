22583a2182973ee9269faec85d86465237adfcb3
AJ Chan <adam@ajchan.io>
2024-05-24 22:25:49 -0700

    Add inference endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -25,13 +25,6 @@ def receive_data():
 
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
-        json = request.json
-
-        data = {
-            "source": json.get('source'),
-            "url":  json.get('url'),
-            "content": json.get('content')
-        }
 
         client = weaviate.connect_to_wcs(
             cluster_url=os.getenv("WCS_URL"),
@@ -42,10 +35,8 @@ def receive_data():
         )
 
         try:
-    
             ZoomWorkplaceData = client.collections.get("ZoomWorkplaceData")
-
-            uuid = ZoomWorkplaceData.data.insert(data)
+            uuid = ZoomWorkplaceData.data.insert(request.json)
 
         finally:
             client.close()  # Close client gracefully
@@ -55,3 +46,31 @@ def receive_data():
     else:
         return 'Content-Type not supported!'
 
+
+@app.route('/query', methods=['POST'])
+def query_data(): 
+
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+
+        client = weaviate.connect_to_wcs(
+            cluster_url=os.getenv("WCS_URL"),
+            auth_credentials=weaviate.auth.AuthApiKey(os.getenv("WCS_API_KEY")),
+            headers={
+                "X-OpenAI-Api-Key": os.environ["OPENAI_APIKEY"]  # Replace with your inference API key
+            }
+        )
+
+        try:
+            ZoomWorkplaceData = client.collections.get("ZoomWorkplaceData")
+            response = ZoomWorkplaceData.generate.hybrid(query=request.json.get('message'),
+                limit=4,
+                grouped_task=f"You are a helpful AI Assistant. Be brief in your responses. Write a reply to a chat message in reply to the following data. Here's the user's message {request.json.get('message')}",)
+
+        finally:
+            client.close()  # Close client gracefully
+
+        return jsonify({"response": response.generated}), 200
+
+    else:
+        return 'Content-Type not supported!'
